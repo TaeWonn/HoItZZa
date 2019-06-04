38596e34508f639657ac529adca5c2c16d7d229b
kidhof59 <kidhof59@gmail.com>
2019-06-04 12:24:36 +0900

    Commit before Pull

---
+++ TeamProject/src/point/controller/PointChargeServlet.java
@@ -0,0 +1,70 @@
+package point.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import point.model.service.PointService;
+import point.model.vo.Point;
+import user.model.vo.User;
+
+/**
+ * Servlet implementation class PointChargeServlet
+ */
+@WebServlet("/views/point/pointView")
+public class PointChargeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PointChargeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 파라미터 핸들링
+		String userId = ((User)request.getSession().getAttribute("userLoggedIn")).getUserId();
+		
+		// 2. 업무 로직
+		List<Point> list = new PointService().selectChargeListById(userId);
+		
+		// 3. view단 처리
+		String view = "";
+		String msg = "";
+		String loc = "";
+		if(userId==null && "".equals(userId)) {
+			view = "/WEB-INF/views/common/msg.jsp";
+			msg = "회원 아이디를 찾을 수 없습니다.";
+			loc = "/";
+			
+			request.setAttribute("msg", msg);
+			request.setAttribute("loc", loc);
+		}
+		else {
+			view = "/WEB-INF/views/point/pointCharge.jsp";
+		}
+		
+		request.getRequestDispatcher(view)
+			   .forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- TeamProject/src/point/model/service/PointService.java
+++ TeamProject/src/point/model/service/PointService.java
@@ -1,5 +1,14 @@
 package point.model.service;
 
+import java.util.List;
+
+import point.model.vo.Point;
+
 public class PointService {
+
+	public List<Point> selectChargeListById(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
