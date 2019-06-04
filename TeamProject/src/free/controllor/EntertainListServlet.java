277c147c9c2ab688930cf486d21d4e2f7b5bd5b1
kidhof59 <kidhof59@gmail.com>
2019-06-05 00:23:27 +0900

    게시판 연결 서블릿
    잡담게시판을 만들어두고 생활상식 게시판을 안 만들어두어서
    차후 이 부분 수정을 정우영팀원에게 컨펌.

--- TeamProject/src/free/controllor/EntertainListServlet.java
+++ TeamProject/src/free/controllor/EntertainListServlet.java
@@ -8,18 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class DivideListServlet
+ * Servlet implementation class EntertainListServlet
  */
-@WebServlet("/free/divideList")
-public class DivideListServlet extends HttpServlet {
+@WebServlet("/free/entertainList")
+public class EntertainListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/free/divideList.jsp")
-			   .forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/free/entertainList.jsp")
+		  	   .forward(request, response);
 	}
 
 	/**
